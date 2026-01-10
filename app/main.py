5c2b080cf2c4fb397099a920f7b91893c7ba5d61
Akhilesh <akhi2431@gmail.com>
2026-01-10 18:35:40 +0000

    API setup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from app.model import dummy_predict
 
 app = FastAPI()
+
+class ResumeRequest(BaseModel):
+    resume_text: str
+    job_description: str
 
 @app.get("/")
 def health_check():
     return {"status": "API is running"}
+
+@app.post("/score_resume")
+def score_resume(data: ResumeRequest):
+    score = dummy_predict(data.resume_text, data.job_description)
+    return {"match_score": round(score * 100, 2)}
---
+++ app/model.py
@@ -0,0 +1,3 @@
+def dummy_predict(resume_text: str, job_text: str) -> float:
+    # Placeholder logic
+    return 0.75
